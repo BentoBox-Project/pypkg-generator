ee7031753d7511a26dde63fc25863357cfc11e31
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2019-10-03 21:39:23 -0500

    Fixed pytest fixtures in test suite

--- tests/test_package_creator.py
+++ tests/test_package_creator.py
@@ -7,21 +7,21 @@ from pypkg_generator import package_generator
 from pypkg_generator import exceptions
 
 
-@pytest.fixture()
+@pytest.fixture
 def dir():
     with TempDirectory() as dir:
         yield dir
 
 
-def pkg_generator(dir, pkg_name):
-    args = {'name': pkg_name, 'path': dir.path}
+@pytest.fixture
+def pkg_generator(dir):
+    args = {'name': 'pkg_name', 'path': dir.path}
     return package_generator.PackageGenerator(args)
 
 
-def test_create_new_package(dir):
-    creator = pkg_generator(dir, 'pkg_name')
-    creator.call()
-    assert os.path.exists(os.path.join(dir.path, creator.package_name))
+def test_create_new_package(dir, pkg_generator):
+    pkg_generator.call()
+    assert os.path.exists(os.path.join(dir.path, pkg_generator.package_name))
 
 
 def tests_no_name_error(dir):
@@ -32,9 +32,8 @@ def tests_no_name_error(dir):
     assert str(e.value) == 'The name is empty!'
 
 
-def tests_dir_existst_error(dir):
+def tests_dir_existst_error(dir, pkg_generator):
     with pytest.raises(exceptions.DirectoryExistsError) as e:
-        creator = pkg_generator(dir, 'pkg_name')
-        creator.call()
-        creator._create_dir('pkg_name')
+        pkg_generator.call()
+        pkg_generator._create_dir('pkg_name')
     assert str(e.value) == 'The directory already exists'
